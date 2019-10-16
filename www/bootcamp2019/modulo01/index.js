611317fe3dbbc091fc35e962ae505d50da5a70d8
Vinicius Moraes da Silva <vinicius.moraes.silva@gmail.com>
2019-10-16 08:24:05 -0300

    Modulo 01 Finalizado - Ambiente e Conceitos

--- www/bootcamp2019/modulo01/index.js
+++ www/bootcamp2019/modulo01/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "name": "Vinicius", "email": "vinicius.moraes.silva@gmail.com"}
@@ -11,13 +13,69 @@ const server = express();
 
 // CRUD - Create, Read, Update, Delete
 
-const users = [ 'Diego', 'Claudio', 'Vinicius'];
+const users = ['Diego', 'Claudio', 'Vinicius'];
 
-server.get('/users/:index', (req, res) => {
+server.use((req, res, next) => {
+  console.time('Request');
+  console.log(`Metodo: ${req.method}; URL: ${req.url}`);
+
+  next();
+
+  console.timeEnd('Request');
+});
+
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ erro: 'User names is required' });
+  }
+  return next();
+};
+
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+
+  if (!user) {
+    return res.status(400).json({ error: 'User does not exists' });
+  }
+  
+  req.user = user;
+
+  return next();
+}
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+server.get('/users/:index', checkUserInArray, (req, res) => {
   // const nome = req.query.nome; Query Exemplo
+  return res.json(req.user);
+});
+
+server.post('/users', checkUserExists, (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', checkUserInArray, (req, res) => {
   const { index } = req.params;
 
-  return res.json(users[index]);
-});
+  users.splice(index, 1);
+
+  return res.send();
+})
 
 server.listen(3000);
